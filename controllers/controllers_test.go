0b579e5f43eb32c5540fa8e6209e608d192c8f8c
keijoraamat <raamtkeijo@gmail.com>
2023-05-02 21:39:28 +0300

    Finding test deeper

--- controllers/controllers_test.go
+++ controllers/controllers_test.go
@@ -47,47 +47,34 @@ func cleanup() {
 func seedDB() {
 	var acts = make([]models.FindingAct, 3)
 
-	var one = &models.FindingAct{
-		FinderName:       "Mari Maasikas",
-		FinderIdNumber:   "EE12345667890",
-		RecieverName:     "Tuule Tallerma",
-		FindingType:      "juhu",
-		FindersFee:       false,
-		ResiginOwnership: true,
-		RemainAnonymous:  false,
-		TransferLocation: "Fellin",
-		WDActNumber:      "33.66453",
-		Status:           "töös",
-	}
-	var two = &models.FindingAct{
-		FinderName:       "Mati Kurikas",
-		FinderIdNumber:   "EE2345678900",
-		RecieverName:     "Tuule Tallerma",
-		FindingType:      "detektor",
-		FindersFee:       true,
-		ResiginOwnership: true,
-		RemainAnonymous:  false,
-		TransferLocation: "Dorpat",
-		WDActNumber:      "33.234523",
-		Status:           "arhiiv",
-	}
-	var three = &models.FindingAct{
-		FinderName:       "Mari Maasikas",
-		FinderIdNumber:   "EE12345667890",
-		RecieverName:     "Tuuve Tallerma",
-		FindingType:      "detektor",
-		FindersFee:       false,
-		ResiginOwnership: true,
-		RemainAnonymous:  false,
-		TransferLocation: "Lindanise",
-		WDActNumber:      "44.66453",
-		Status:           "töös",
-	}
-
-	acts[0] = *one
-	acts[1] = *two
-	acts[2] = *three
+	acts[0] = *firstFindingAct
+	acts[1] = *secondFindingAct
 
 	db.Create(&acts)
 
+}
+
+var firstFindingAct = &models.FindingAct{
+	FinderName:       "Mari Maasikas",
+	FinderIdNumber:   "EE12345667890",
+	RecieverName:     "Tuule Tallerma",
+	FindingType:      "juhu",
+	FindersFee:       false,
+	ResiginOwnership: true,
+	RemainAnonymous:  false,
+	TransferLocation: "Fellin",
+	WDActNumber:      "33.66453",
+	Status:           "töös",
+}
+var secondFindingAct = &models.FindingAct{
+	FinderName:       "Mati Kurikas",
+	FinderIdNumber:   "EE2345678900",
+	RecieverName:     "Tuule Tallerma",
+	FindingType:      "detektor",
+	FindersFee:       true,
+	ResiginOwnership: true,
+	RemainAnonymous:  false,
+	TransferLocation: "Dorpat",
+	WDActNumber:      "33.234523",
+	Status:           "arhiiv",
 }
--- controllers/findings_test.go
+++ controllers/findings_test.go
@@ -1,6 +1,7 @@
 package controllers_test
 
 import (
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -12,7 +13,7 @@ import (
 )
 
 func TestFindings_Index_Should_Contain_List_Of_Findig_Acts(t *testing.T) {
-	engine := html.New(".. /views", ".tmpl")
+	engine := html.New("../views", ".tmpl")
 
 	app := fiber.New(fiber.Config{
 		Views: engine,
@@ -30,5 +31,15 @@ func TestFindings_Index_Should_Contain_List_Of_Findig_Acts(t *testing.T) {
 	}
 
 	assert.Equal(t, http.StatusOK, res.StatusCode)
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Contains(t, string(body), firstFindingAct.WDActNumber)
+	assert.Contains(t, string(body), firstFindingAct.FinderIdNumber)
+	assert.Contains(t, string(body), secondFindingAct.FinderIdNumber)
+	assert.Contains(t, string(body), secondFindingAct.FinderIdNumber)
 
 }
